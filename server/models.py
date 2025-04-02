35c09e463f751a6dda95307cc5f604434d2aa1db
VictorIgati <victor.igati@student.moringaschool.com>
2025-04-02 13:02:23 +0300

    Add validation for Author and Post models

--- server/models.py
+++ server/models.py
@@ -1,17 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy.exc import IntegrityError
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
     
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author must have a name")
+        
+        # Check if another author with this name already exists
+        existing_author = Author.query.filter(Author.name == name).first()
+        if existing_author:
+            raise ValueError("Author name must be unique")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, number):
+        if number and (len(number) != 10 or not number.isdigit()):
+            raise ValueError("Phone number must be exactly 10 digits")
+        return number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +43,34 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Post must have a title")
+        
+        clickbait_words = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(word in title for word in clickbait_words):
+            raise ValueError("Title must contain clickbait words")
+        return title
 
+    @validates('content')
+    def validate_content(self, key, content):
+        if content and len(content) < 250:
+            raise ValueError("Content must be at least 250 characters")
+        return content
+
+    @validates('category')
+    def validate_category(self, key, category):
+        valid_categories = ["Fiction", "Non-Fiction"]
+        if category and category not in valid_categories:
+            raise ValueError("Category must be either Fiction or Non-Fiction")
+        return category
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Summary must not exceed 250 characters")
+        return summary
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
